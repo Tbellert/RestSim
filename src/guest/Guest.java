10123711515f6ac987d18bb8724e3e9e79046a09
Timo Bellert <t.bellert@workingspirit.nl>
2023-02-02 10:48:46 +0100

    First commit

--- src/guest/Guest.java
+++ src/guest/Guest.java
@@ -1,5 +1,16 @@
 package guest;
 
-public class Guest {
-
+public abstract class Guest {
+	GuestProfile guestProfile;
 }
+
+//Vraag aan mijn mede developer:
+//Idee is als volgt.
+//Mijn app gaat in het Reserveringssysteem reserveringen ophalen per dag van de week gebaseerd op een bezettingsgraad per dag.
+//Die reserveringen zijn voor een random aantal gasten met een bepaald gastenprofiel welke ik uit een een array met data wil halen (denk aan een array met namen, achternamen, leeftijd, favoriet eten, etc etc)
+
+// Maak een class aan met de naam MedeDeveloper die Guest extends. (zoals GuestProfile)
+// Voeg aan deze class een aantal fields toe en schrijf een methode die van toepassing zou kunnen zijn op jou, een developer of een gast in een restaurant.
+// Mocht het later toch  niet aansluiten met mijn vereisten dan overschrijf ik dit wel.
+
+// Op deze manier kunnen jullie als gast in het restaurant ontvangen worden (Als het goed is).
