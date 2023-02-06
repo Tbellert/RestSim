a6da55b0cc922757bd90de574e71f54cf3561b03
Timo Bellert <t.bellert@workingspirit.nl>
2023-02-06 16:08:03 +0100

    Reservations and weekly, montlhy, yearly updates

--- src/System/Reservation.java
+++ src/System/Reservation.java
@@ -6,12 +6,18 @@ import guest.Guest;
 import guest.GuestProfile;
 
 public class Reservation {
-	ArrayList<Guest> guests = new ArrayList<>();
-	
+	public String name;
+	public int numberOfGuests;
+	public ArrayList<Guest> guests = new ArrayList<>();
+		
 	public Reservation(int randomNumber) {
 		for (int i = randomNumber; i>0 ; i--) {
+			this.numberOfGuests = randomNumber;
 			Guest guest = new GuestProfile();
 			guests.add(guest);
+			if (i == 1) {
+				this.name = guest.getLastname();
+			}
 		}
 	}
 }
